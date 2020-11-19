b68a0adf8aa694ea6e2058e2d03d524716b37e5d
Alexandra Zaitsava <ilit_alex@tut.by>
2020-11-19 22:31:17 +0300

    init: start project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,123 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import { moviesData } from "./moviesData";
+import MovieItem from "./MovieItem";
+import { API_URL, API_KEY_3 } from "../src/utils/API";
+import  MovieTabs  from "./MovieTabs";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      movies: [],
+      moviesWillWatch: [],
+      sort_by: "popularity.desc",
+    };
+  }
+  componentDidMount() {
+    this.getMovies()
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    if (prevState.sort_by !== this.state.sort_by) {
+      this.getMovies()
+    }
+
+  }
+
+  getMovies = () => {
+    fetch(`${API_URL}/discover/movie?api_key=${API_KEY_3}&sort_by=${this.state.sort_by}&page=${this.state.page}`).then((response) =>   {
+      return response.json();
+
+    }).then((data) => {
+      this.setState({
+        movies: data.results,
+      })
+    })
+  }
+
+  deleteMovie = movie => {
+    const updateMovies = this.state.movies.filter(item => item.id !== movie.id);
+
+    this.setState({
+      movies: updateMovies
+    });
+  };
+
+  addMovieToWillWatch = movie => {
+    const updateMoviesWillWatch = [...this.state.moviesWillWatch];
+    updateMoviesWillWatch.push(movie);
+
+    this.setState({
+      moviesWillWatch: updateMoviesWillWatch
+    });
+  };
+
+  deleteMovieFromWillWatch = movie => {
+    const updateMoviesWillWatch = this.state.moviesWillWatch.filter(
+        item => item.id !== movie.id
+    );
+
+    this.setState({
+      moviesWillWatch: updateMoviesWillWatch
+    });
+  };
+
+  updateSortBy = value => {
+    this.setState({
+      sort_by: value,
+    })
+  }
+
+
+
+  render() {
+    return (
+        <div className="container">
+          <div className="row mt-4">
+            <div className="col-9">
+              <div className="row mb-4">
+                <div className="col-12">
+                  < MovieTabs
+                      sort_by={this.state.sort_by}
+                      updateSortBy={this.updateSortBy}
+                  />
+                </div>
+              </div>
+              <div className="row">
+                {this.state.movies.map(movie => {
+                  return (
+                      <div className="col-6 mb-4" key={movie.id}>
+                        <MovieItem
+                            data={movie}
+                            deleteMovie={this.deleteMovie}
+                            addMovieToWillWatch={this.addMovieToWillWatch}
+                            deleteMovieFromWillWatch={this.deleteMovieFromWillWatch}
+                        />
+                      </div>
+                  );
+                })}
+              </div>
+            </div>
+            <div className="col-3">
+              <h4>Will Watch: {this.state.moviesWillWatch.length} movies</h4>
+              <ul className="list-group">
+                {this.state.moviesWillWatch.map(movie => (
+                    <li key={movie.id} className="list-group-item">
+                      <div className="d-flex justify-content-between">
+                        <p>{movie.title}</p>
+                        <p>{movie.vote_average}</p>
+                      </div>
+                    </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        </div>
+    );
+  }
 }
 
 export default App;
+
